edce0051337f9d34cfc0ce906cab41c401ef9607
yaxyobekuz <pubgn9642@gmail.com>
2026-03-24 21:57:23 +0500

    Add animation assets for emojis and ducks

---
+++ src/shared/assets/animations/index.js
@@ -0,0 +1,37 @@
+// Emoji Animations
+import tvEmojiAnimation from "./emoji/tv.json";
+import timeEmojiAnimation from "./emoji/time.json";
+import gcapEmojiAnimation from "./emoji/gcap.json";
+import adminEmojiAnimation from "./emoji/admin.json";
+import magicEmojiAnimation from "./emoji/magic.json";
+import folderEmojiAnimation from "./emoji/folder.json";
+import teacherEmojiAnimation from "./emoji/teacher.json";
+import greetingEmojiAnimation from "./emoji/greeting.json";
+import statsBarEmojiAnimation from "./emoji/stats-bar.json";
+import lockWithKeyEmojiAnimation from "./emoji/lock-with-key.json";
+
+// Duck Animations
+import pedroDuckAnimation from "./duck/pedro.json";
+import coolmanDuckAnimation from "./duck/coolman.json";
+import greetingDuckAnimation from "./duck/greeting.json";
+import blowingKissDuckAnimation from "./duck/blowing-kiss.json";
+
+export {
+  // Emoji Animations
+  tvEmojiAnimation,
+  gcapEmojiAnimation,
+  timeEmojiAnimation,
+  adminEmojiAnimation,
+  magicEmojiAnimation,
+  folderEmojiAnimation,
+  teacherEmojiAnimation,
+  greetingEmojiAnimation,
+  statsBarEmojiAnimation,
+  lockWithKeyEmojiAnimation,
+
+  // Duck Animations
+  pedroDuckAnimation,
+  coolmanDuckAnimation,
+  greetingDuckAnimation,
+  blowingKissDuckAnimation,
+};
